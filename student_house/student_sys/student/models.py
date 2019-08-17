d8a05146e95cd1dfdc9839fbdad30048f42fddc0
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-17 19:38:24 +0800

    test

--- student_house/student_sys/student/models.py
+++ student_house/student_sys/student/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
 
 class Student (models.Model):
     SEX_ITEMS = [
-        (),
-        (),
-        (),
+        (0, 'unknow'),
+        (1, 'man'),
+        (2, 'woman'),
     ]
+    STATUS_ITEM = [
+        (0, 'application'),
+        (1, 'pass'),
+        (2, 'refuse'),
+    ]
+    name = models.CharField(max_length=128, verbose_name='name')
 

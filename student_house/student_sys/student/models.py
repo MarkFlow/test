b863d38de280a4966e8fd24a7392d0f0ef060024
mark-laptop <baoyinzhe8810022hotmail.com>
2019-08-17 22:00:05 +0800

    admin

--- student_house/student_sys/student/models.py
+++ student_house/student_sys/student/models.py
@@ -4,13 +4,28 @@ from django.db import models
 class Student (models.Model):
     SEX_ITEMS = [
         (0, 'unknow'),
-        (1, 'man'),
-        (2, 'woman'),
+        (1, 'male'),
+        (2, 'female'),
     ]
     STATUS_ITEM = [
         (0, 'application'),
         (1, 'pass'),
         (2, 'refuse'),
     ]
-    name = models.CharField(max_length=128, verbose_name='name')
+    name = models.CharField(max_length=128, verbose_name='姓名')
+    sex = models.IntegerField(choices=SEX_ITEMS, verbose_name='性别')
+    profession = models.CharField(max_length=128, verbose_name='职业')
+    email = models.EmailField(verbose_name='邮箱')
+    qq = models.CharField(max_length=128, verbose_name='QQ')
+    phone = models.CharField(max_length=128, verbose_name='电话')
+
+    status = models.IntegerField(choices=STATUS_ITEM, default=0, verbose_name='审核状态')
+    created_item = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='创建时间')
+
+    def __str__(self):
+        return '<Student: {}>'.format(self.name)
+
+    class Meta:
+        verbose_name = verbose_name_plural = '学员信息'
+
 
